3c27dd7bec78e9109b5cb1b1206a53436cec2fc3
alexgicimo <gakurualexgicimo@gmail.com>
2022-10-22 13:28:52 +0300

    primitives in java

---
+++ src/Primitives.java
@@ -0,0 +1,9 @@
+class Primitives{
+    public static void main(String[] args) {
+        int myValue = 1000;
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum Value : " +myMinIntValue);
+        System.out.println("Integer Minimum Value : " +myMaxIntValue);
+    }
+}

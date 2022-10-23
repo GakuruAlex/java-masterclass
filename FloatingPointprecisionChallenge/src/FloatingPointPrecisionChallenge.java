b8aebdddce1444b243242cd08a35519330a49761
alexgicimo <gakurualexgicimo@gmail.com>
2022-10-23 13:26:08 +0300

    Floating poing precision in java

---
+++ FloatingPointprecisionChallenge/src/FloatingPointPrecisionChallenge.java
@@ -0,0 +1,9 @@
+public class FloatingPointPrecisionChallenge {
+    public static void main(String[] args) {
+        double poundToKilo = 0.45359237;
+        double myPound = 100d;
+        double myKilograms = myPound * poundToKilo ;
+        System.out.println("100 pounds are equivalent to " + myKilograms + " kilograms ");
+
+    }
+}
